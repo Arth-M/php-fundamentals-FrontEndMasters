c6dcb1108ee8cbcee5d26c3907395cb0932b0c5c
Arthur-Henri Michalland <ah.michalland@gmail.com>
2025-03-11 11:04:52 +0100

    great language to learn i believe!

--- playground/test.php
+++ playground/test.php
@@ -1,9 +1,20 @@
 <?php
-$name = "Arthuro"
-echo "Hello world"
-echo $name
-?>
- <!-- php test.php will launch the code in your terminal -->
+// CLI 'php test.php' will launch the code in your terminal
 
- You can also write output here ! Be careful when writing scripts not to add
+//this is a single line commentary
+
+$name = "Arthuro";
+echo "Hello world";
+
+echo("Hello woorld!");
+echo $name;
+
+/* this is a multiline
+commentary */
+?>
+
+// comments does not work outside '<?php ?> ' tags as it is considered an output
+
+
+ So you can also write output outside of php tags ! Be careful when writing scripts not to add
  spaces or anything after the closing tag that you don't want to be part of the output!
