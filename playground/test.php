4908548aba3376950317cd1c5bda0786663eb606
Arthur-Henri Michalland <ah.michalland@gmail.com>
2025-03-11 10:58:58 +0100

    in progress for php understanding

--- playground/test.php
+++ playground/test.php
@@ -1,4 +1,9 @@
 <?php
+$name = "Arthuro"
 echo "Hello world"
-
+echo $name
 ?>
+ <!-- php test.php will launch the code in your terminal -->
+
+ You can also write output here ! Be careful when writing scripts not to add
+ spaces or anything after the closing tag that you don't want to be part of the output!
