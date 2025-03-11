d31237ce5a7a649ac8c162ed1755579d36861dc4
Arthur-Henri Michalland <ah.michalland@gmail.com>
2025-03-11 15:56:57 +0100

    in progress

--- playground/test.php
+++ playground/test.php
@@ -8,6 +8,7 @@ echo "Hello world";
 echo "\n";
 echo("Hello woorld!");
 echo $name;
+echo strlen($name);
 
 /* this is a multiline
 commentary */
@@ -22,14 +23,11 @@ $$varname = "lesson 1"; //this is a variable variable
 echo $varname;
 echo "\n";
 echo $Teaching;
-/* this is a closing php tag :
-?>
-You can avoid it to prevent unwanted output from your script
-*/
+
 $last_year = 2024;
 echo "last year is $last_year";
 echo "\n";
-echo "New year is {$last_year+1}";
+echo "New year is {($last_year + 1)}";
 echo"\n";
 
 $try_this = <<<TRYITOUT
@@ -41,6 +39,68 @@ $try_this = <<<TRYITOUT
 TRYITOUT;
 
 echo $try_this;
+echo "\n";
+
+$first_array = array(2, 3, 6, 25);
+$second_array = ["coucou", "second_string"=>"bonjour", "ciao", "ohayo?"];
+
+echo($first_array[0]);
+echo($second_array[0]);
+echo($second_array[1]);
+
+echo($second_array["second_string"]);
+echo($second_array[2]);
+
+echo count($second_array);
+echo "\n";
+for ($i=0; $i<count($first_array); $i+=1) {
+  echo "{$first_array[$i]} \n";
+}
+
+foreach ($second_array as $salutation) {
+  echo $salutation . "\n";
+}
+
+foreach ($second_array as $index=>$salutation) {
+  echo "{$index}: {$salutation}" . "\n";
+}
+
+function print_my_first_function() {
+  echo 'This is my first function';
+  echo "\n";
+}
+
+print_my_first_function();
+
+//variable type (without OOP class consideration) :
+// bool, int, float, string, array, object, callable
+// (callable means a function)
+function calculate_tax(float $price, float $tva) { //int and float in case you want to specidy variable type
+  return $price * $tva;
+}
+
+echo calculate_tax(60, 0.20);
+echo "\n";
+// echo calculate_tax(tax:0.20, price:120);
+echo "\n";
+echo calculate_tax($tax=0.20, $price=120);
+
+// i++ & i+=1 sont différents, à checker
+/* this is a closing php tag :
+?>
+You can avoid it to prevent unwanted output from your script
+*/
+
+/*
+************* examine une condition puis exécute l'action
+ while (boolean_condition) {
+
+ }
+
+************* exécute l'action une première fois puis examine la condition avant de réxécuter l'action
+ do {
+ } while (boolean_condition)
+*/
 ?>
 
 comments does not work outside php tags as it is considered an output
