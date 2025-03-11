a3135a8ee7b2c31f95bad2226a0bc495d43ebb23
Arthur-Henri Michalland <ah.michalland@gmail.com>
2025-03-11 11:17:37 +0100

    in progress

--- playground/test.php
+++ playground/test.php
@@ -11,10 +11,19 @@ echo $name;
 
 /* this is a multiline
 commentary */
+
+
+/* this is a closing php tag :
+?>
+You can avoid it to prevent unwanted output from your script
+*/
+
 ?>
 
-// comments does not work outside '<?php ?> ' tags as it is considered an output
+comments does not work outside php tags as it is considered an output
 
 
  So you can also write output outside of php tags ! Be careful when writing scripts not to add
  spaces or anything after the closing tag that you don't want to be part of the output!
+
+ Another way to avoid any problem is not to close the php tag !
