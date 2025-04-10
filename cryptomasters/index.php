31bfb7874fb58b8d52937e659864482931bd5d27
Arthur-Henri Michalland <ah.michalland@gmail.com>
2025-04-10 14:59:00 +0200

    in progress

---
+++ cryptomasters/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Crypto Masters</title>
+</head>
+<body>
+  <h1>Crypto on the way</h1>
+
+  <p>Enter your amount of crypto</p>
+
+  <form action="">
+    <label for="amount">Amount</label>
+    <input type="text" id="amount">
+    <label for="crypto">Cryptocurrency</label>
+    <select name="cryptocurrency" id="crypto">
+      <option value="bitcoin">BTC</option>
+      <option value="ether">ETH</option>
+    </select>
+    <button type="submit">Convert</button>
+  </form>
+  
+</body>
+</html>
