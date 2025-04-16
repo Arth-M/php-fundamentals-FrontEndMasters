c9ecb00f110ce42b3029689de47d43b1673fe675
Arthur-Henri Michalland <ah.michalland@gmail.com>
2025-04-16 10:49:47 +0200

    changed footer and the design pattern used with indexing details in the url is known as master detail navigation with server side rendering

--- frontendmuseum/footer_inc.php
+++ frontendmuseum/footer_inc.php
@@ -1,3 +1,7 @@
-</main>
-</body>
+      </main>
+    </body>
+  <footer>
+    <h1><img src="images/logo.png" alt="Frontend Masters Museum" width="300"
+                fetchpriority="high" style="margin:auto auto auto 70vw"></h1>
+  </footer>
 </html>
